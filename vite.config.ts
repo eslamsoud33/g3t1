d787b5e5f9160f5e021653aac91485959e39386c
EslamSoud <eslamsoud33@gmail.com>
2026-05-24 02:21:28 +0300

    Add files via upload

---
+++ vite.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
+
+export default defineConfig({
+  plugins: [
+    tailwindcss(),
+    react()
+  ],
+  base: '/elkinz/', // تم التعديل ليتطابق مع مجلد ومستودع الكنز الجديد
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom', 'motion'],
+        },
+      },
+    },
+  },
+});
